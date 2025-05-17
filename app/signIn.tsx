23e3419a722ce9909d68ebe7b17ccd89fd9ae430
C0539116 <C0539116@online.camosun.ca>
2025-05-16 19:56:44 -0700

    Add Sign Up, Sign In and lottie-react-native for animations in package.json

--- app/signIn.tsx
+++ app/signIn.tsx
@@ -1,16 +1,152 @@
-import {Text, View } from "react-native";
+import { Text, View, Image, TextInput, TouchableOpacity, Pressable, Alert } from "react-native";
+import React, { useRef, useState } from 'react'
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
+import { StatusBar } from "expo-status-bar";
+import { Octicons } from '@expo/vector-icons';
+import { useRouter } from 'expo-router';
+import Loading from '../components/Loading';
+import CustomKeyboardView from '../components/CustomKeyboardView';
 
-export default function StartPage() {
+
+export default function SingIn() {
+    const router = useRouter();
+    const [loading, setLoadong] = useState(false);
+    const emailRef = useRef("");
+    const passwordRef = useRef("");
+
+    const handleLogin = async () => {
+        if (!emailRef.current || !passwordRef.current) {
+            Alert.alert('Sign In', "Please fill all the fields!");
+            return;
+        }
+    }
 
     return (
-        <View
-            style={{
-            flex: 1,
-            justifyContent: "center",
-            alignItems: "center",
-            }}
-        >
-            <Text>Sign In</Text>
-        </View>
+        <CustomKeyboardView>
+            <StatusBar style="dark" />
+            <View
+                style={{
+                    paddingTop: hp(8),
+                    paddingHorizontal: wp(5),
+                    justifyContent: "flex-start",
+                    alignItems: "center",
+                }}
+            >
+                <View>
+                    <Image style={{ height: hp(25) }} resizeMode='contain' source={require('../assets/images/Login-background/Group6.png')} />
+                </View>
+            </View>
+            <View style={{ 
+                gap: hp(5),
+                width: '100%',
+                alignItems: 'center',
+                }}>
+                <Text style={{
+                    fontSize: hp(4),
+                    fontWeight: 'bold',
+                    letterSpacing: 1,
+                    textAlign: 'center',
+                }}>
+                    Sign In
+                </Text>
+
+                <View style={{ gap: 16 }}>
+                    <View style={{
+                        width: wp(90),
+                        height: hp(7),
+                        gap: 16,
+                        flexDirection: 'row',
+                        alignItems: 'center',
+                        backgroundColor: '#f5f5f5',
+                        borderRadius: 16,
+                        paddingHorizontal: wp(4),
+                    }}>
+                        <Octicons name="mail" size={hp(2.7)} color="gray" />
+                        <TextInput style={{
+                            fontSize: hp(2),
+                            flex: 1,
+                            color: '#404040'
+                        }} placeholder="Email address" placeholderTextColor={'gray'} onChangeText={value => emailRef.current = value} />
+                    </View>
+
+                    <View style={{
+                        gap: 12,
+                    }}>
+                        <View style={{
+                            width: wp(90),
+                            height: hp(7),
+                            gap: 16,
+                            flexDirection: 'row',
+                            alignItems: 'center',
+                            backgroundColor: '#f5f5f5',
+                            borderRadius: 16,
+                            paddingHorizontal: wp(4),
+                        }}>
+                            <Octicons name="lock" size={hp(2.7)} color="gray" />
+                            <TextInput style={{
+                                fontSize: hp(2),
+                                flex: 1,
+                                color: '#404040'
+                            }} placeholder="Password"
+                                secureTextEntry
+                                placeholderTextColor={'gray'}
+                                onChangeText={value => passwordRef.current = value} />
+                        </View>
+                        <Text style={{
+                            fontSize: hp(1.8),
+                            fontWeight: 600,
+                            color: '#737373',
+                            textAlign: 'right'
+                        }}>Forgot password?</Text>
+                    </View>
+
+                    <View>
+                        {
+                            loading ? (
+                                <View style={{
+                                    flexDirection: 'row',
+                                    justifyContent: "center",
+                                }}>
+                                    <Loading size={hp(6.5)} />
+                                </View>
+                            ) : (
+                                <TouchableOpacity onPress={handleLogin} style={{
+                                    backgroundColor: '#6366F1',
+                                    borderRadius: 16,
+                                    justifyContent: 'center',
+                                    alignItems: "center",
+                                    height: hp(6.5),
+                                }}>
+                                    <Text style={{
+                                        fontSize: hp(2.7),
+                                        fontWeight: 700,
+                                        color: '#ffffff',
+                                        letterSpacing: 1,
+                                    }}>Sign In
+                                    </Text>
+                                </TouchableOpacity>
+                            )
+                        }
+                    </View>
+
+                    <View style={{
+                        flexDirection: 'row',
+                        justifyContent: 'center',
+                        flexWrap: 'wrap',
+                        width: wp(90),
+                    }}>
+                        <Text style={{ width: wp(45), textAlign: 'center', fontSize: hp(1.8), fontWeight: '600', color: '#737373', }}>
+                            Don't have an account?
+                        </Text>
+
+                        <Pressable onPress={() => router.push('/signUp')}>
+                            <Text style={{ width: wp(45), textAlign: 'center', fontSize: hp(1.8), fontWeight: '600', color: '#6366F1', }}>
+                                Sign Up
+                            </Text>
+                        </Pressable>
+                    </View>
+                </View>
+            </View>
+        </CustomKeyboardView >
     );
 }
---
+++ app/signUp.tsx
@@ -0,0 +1,196 @@
+import { Text, View, Image, TextInput, TouchableOpacity, Pressable, Alert } from "react-native";
+import React, { useRef, useState } from 'react'
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
+import { StatusBar } from "expo-status-bar";
+import { Octicons, Feather } from '@expo/vector-icons';
+import { useRouter } from 'expo-router';
+import Loading from '../components/Loading';
+import CustomKeyboardView from '../components/CustomKeyboardView';
+
+
+export default function SingUp() {
+  const router = useRouter();
+  const [loading, setLoadong] = useState(false);
+  const emailRef = useRef("");
+  const passwordRef = useRef("");
+  const usernameRef = useRef("");
+  const profileRef = useRef("");
+
+  const handleRegister = async () => {
+    if (!emailRef.current || !passwordRef.current || !usernameRef.current || !profileRef.current) {
+      Alert.alert('Sign Up', "Please fill all the fields!");
+      return;
+    }
+  }
+
+  
+  return (
+    <CustomKeyboardView>
+      <StatusBar style="dark" />
+      <View
+        style={{
+          paddingTop: hp(7),
+          paddingHorizontal: wp(5),
+          justifyContent: "flex-start",
+          alignItems: "center",
+         }}
+      >
+        <View>
+          <Image style={{ height: hp(20) }} resizeMode='contain' source={require('../assets/images/Login-background/Group6.png')} />
+        </View>
+      </View>
+
+      <View style={{
+        gap: hp(5),
+        width: wp(90),
+        alignSelf: 'center',
+        alignItems: 'center',
+        }}>
+        <Text style={{
+          fontSize: hp(4),
+          fontWeight: 'bold',
+          letterSpacing: 1,
+          textAlign: 'center',
+        }}>
+          Sign Up
+        </Text>
+
+        <View style={{ gap: 16 }}>
+
+          <View style={{
+            width: wp(90),
+            height: hp(7),
+            gap: 16,
+            flexDirection: 'row',
+            alignItems: 'center',
+            backgroundColor: '#f5f5f5',
+            borderRadius: 16,
+            paddingHorizontal: wp(4),
+          }}>
+            <Feather name="user" size={hp(2.7)} color="gray" />
+            <TextInput style={{
+              fontSize: hp(2),
+              flex: 1,
+              color: '#404040'
+            }} placeholder="Username" placeholderTextColor={'gray'} onChangeText={value => usernameRef.current = value} />
+          </View>
+
+            <View style={{
+            width: wp(90),
+            height: hp(7),
+            gap: 16,
+            flexDirection: 'row',
+            alignItems: 'center',
+            backgroundColor: '#f5f5f5',
+            borderRadius: 16,
+            paddingHorizontal: wp(4),
+          }}>
+            <Octicons name="mail" size={hp(2.7)} color="gray" />
+            <TextInput style={{
+              fontSize: hp(2),
+              flex: 1,
+              color: '#404040'
+            }} placeholder="Email address" placeholderTextColor={'gray'} onChangeText={value => emailRef.current = value} />
+          </View>
+
+          <View style={{
+            width: wp(90),
+            height: hp(7),
+            gap: 16,
+            flexDirection: 'row',
+            alignItems: 'center',
+            backgroundColor: '#f5f5f5',
+            borderRadius: 16,
+            paddingHorizontal: wp(4),
+          }}>
+            <Octicons name="lock" size={hp(2.7)} color="gray" />
+            <TextInput style={{
+              fontSize: hp(2),
+              flex: 1,
+              color: '#404040'
+            }} placeholder="Password"
+              secureTextEntry
+              placeholderTextColor={'gray'}
+              onChangeText={value => passwordRef.current = value} />
+          </View>
+
+            <View style={{
+            width: wp(90),
+            height: hp(7),
+            gap: 16,
+            flexDirection: 'row',
+            alignItems: 'center',
+            backgroundColor: '#f5f5f5',
+            borderRadius: 16,
+            paddingHorizontal: wp(4),
+          }}>
+            <Feather name="image" size={hp(2.7)} color="gray" />
+            <TextInput style={{
+              fontSize: hp(2),
+              flex: 1,
+              color: '#404040'
+            }} placeholder="Profile url" placeholderTextColor={'gray'} onChangeText={value => profileRef.current = value} />
+          </View>
+
+          <View>
+            {
+              loading ? (
+                <View style={{
+                  flexDirection: 'row',
+                  justifyContent: "center",
+                }}>
+                  <Loading size={hp(6.5)} />
+                </View>
+              ) : (
+                <TouchableOpacity onPress={handleRegister} style={{
+                  backgroundColor: '#6366F1',
+                  borderRadius: 16,
+                  justifyContent: 'center',
+                  alignItems: "center",
+                  height: hp(6.5),
+                }}>
+                  <Text style={{
+                    fontSize: hp(2.7),
+                    fontWeight: 700,
+                    color: '#ffffff',
+                    letterSpacing: 1,
+
+
+                  }}>Sign Up
+                  </Text>
+                </TouchableOpacity>
+              )
+
+            }
+
+          </View>
+
+
+
+          <View style={{
+            flexDirection: 'row',
+            justifyContent: 'center',
+            flexWrap: 'wrap',
+            width: wp(90),
+
+          }}>
+            <Text style={{ width: wp(50), textAlign: 'center', fontSize: hp(1.8), fontWeight: '600', color: '#737373', }}>
+              Already have an account? 
+            </Text>
+
+            <Pressable onPress={() => router.push('/signIn')}>
+              <Text style={{ width: wp(40), textAlign: 'center', fontSize: hp(1.8), fontWeight: '600', color: '#6366F1', }}>
+                Sign in
+              </Text>
+            </Pressable>
+
+
+          </View>
+
+        </View>
+
+      </View>
+
+    </CustomKeyboardView>
+  );
+}
