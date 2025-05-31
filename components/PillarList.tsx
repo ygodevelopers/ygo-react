d51c64508006377fc7f0bd4979e24d0ab7fa1a6d
jimmysday <156743173+jimmysday@users.noreply.github.com>
2025-05-30 23:56:24 -0700

    move the pillar fetch mothed to context previder

--- components/PillarList.tsx
+++ components/PillarList.tsx
@@ -1,45 +1,20 @@
 import { ActivityIndicator, Text, View, FlatList, StyleSheet} from "react-native";
 import { useEffect, useState } from "react";
 
-import { useAuth } from '@/context/authContext'
 import { StatusBar } from 'expo-status-bar'
-import { getDocs, query, where } from 'firebase/firestore'
-import { pillarRef } from '@/firebaseConfig'
+import { useAuth } from '@/context/authContext'
 import PillarItems from '@/components/PillarItems'
+import {usePillar} from '@/context/pillarContext'
 
 export const PillarList = () => {
     const {user} = useAuth();
-    const [Pillars, setPillars] = useState<Pillar[]>([]);
-
-    interface Pillar {
-        color: string;   
-        icon: string;       
-        id: string; 
-        subPillars: string[];
-        title: string;
-        type: string;
-        userId: string;
-        // Add other fields as needed
-    }
-
+    const {getPillars, Pillars} = usePillar();
     useEffect(()=>{
         console.log("PillarList: ", Pillars)
         if(user?.id){
             getPillars();
         }
     },[])
-
-    const getPillars = async()=>{
-        //fetch pillars
-        const q = query(pillarRef, where('userId','==',user?.id));
-        const querySnapshot = await getDocs(q);
-        let data:Pillar[] = [];
-        querySnapshot.forEach(doc => {
-            data.push({...doc.data() as Pillar});
-        })
-        console.log("fetch Pillars: ", data);
-        setPillars(data);
-    }
 
     const combinedData = [...Pillars, { icon: '➕', title: 'New Pillar' }];
 
--- context/pillarContext.js
+++ context/pillarContext.js
@@ -1,12 +1,40 @@
 import { createContext, useContext, useEffect, useState } from "react";
-
+import { getDocs, query, where } from 'firebase/firestore'
+import { pillarRef } from '@/firebaseConfig'
+import { useAuth } from '@/context/authContext'
 
 export const PillarContext = createContext();
-
 export const PillarContextProvider = ({children}) => {
     const [selectedColor, setSelectedColor] = useState("orange");
     const [pillarname, setpillarName] = useState('');
     const [selectedicon,setselectedicon] = useState('📸');
+    const {user} = useAuth();
+
+    // const [Pillars, setPillars] = useState<Pillar[]>([]);
+    // interface Pillar {
+    //     color: string;   
+    //     icon: string;       
+    //     id: string; 
+    //     subPillars: string[];
+    //     title: string;
+    //     type: string;
+    //     userId: string;
+    //     // Add other fields as needed
+    // }
+    const [Pillars, setPillars] = useState([]);
+
+    const getPillars = async()=>{
+        //fetch pillars
+        const q = query(pillarRef, where('userId','==',user?.id));
+        const querySnapshot = await getDocs(q);
+        let data = [];
+        querySnapshot.forEach(doc => {
+            data.push({...doc.data() });
+        })
+        console.log("fetch Pillars: ", data);
+        setPillars(data);
+    }
+    
 
     return (
         <PillarContext.Provider value={{
@@ -15,7 +43,10 @@ export const PillarContextProvider = ({children}) => {
             pillarname, 
             setpillarName,
             selectedicon,
-            setselectedicon
+            setselectedicon,
+            Pillars,
+            setPillars,
+            getPillars
             }}>
             {children}
         </PillarContext.Provider>
