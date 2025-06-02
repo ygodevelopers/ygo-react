c4a8c1f6b89003d8fed7b7b5ebbb1c0e9c3d90ee
John Mullaney <j.a.mullaney1@gmail.com>
2025-06-02 11:53:06 -0700

    feat: Add bottomsheet popup to change pillar

--- app/(app)/ContactView.tsx
+++ app/(app)/ContactView.tsx
@@ -4,22 +4,30 @@ import { contactCollection, userRef } from "@/firebaseConfig";
 import { User, Contact } from "@/types";
 import { useLocalSearchParams } from "expo-router";
 import { getDocs, query, where } from "firebase/firestore";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useMemo, useRef, useState } from "react";
 import { View, Text } from "react-native";
 import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
 import { ContactOption } from "@/components/ContactOption";
 import FontAwesome from "@expo/vector-icons/FontAwesome";
 import FontAwesome5 from "@expo/vector-icons/build/FontAwesome5";
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
+import BottomSheet, {BottomSheetView, BottomSheetBackdrop} from '@gorhom/bottom-sheet';
 
-// TODO: Look into Tanstack Query for state management. Dont really want to pass the threadID or the contactID between pages and then query them for each time i need it. Violates dry.
-// The information also won't change as you cannot change a contacts information, they set it themselves. Would rather store user information in local storage somehow and then delete it once user changes? Or is it better to pull it each time?
 
-// Still reading about Tanstack query but want to get this working before hyper-fixating. 
+
 export default function ContactView() {
     const {contactID} = useLocalSearchParams();
     const [contact, setContact] = useState<User>();
     const [contactPillars, setContactPillars] = useState<Contact>();
     const {user} = useAuth();
+
+
+    const bottomSheetRef = useRef<BottomSheet>(null);
+    const snapPoints = useMemo(() => ["40%"], []);
+    const handlePresentPress = useCallback(() => {
+        console.log("trying to open bottomsheet");
+        bottomSheetRef.current?.expand();
+    }, []);
 
     useEffect(() => {
         getContactInfo();
@@ -41,10 +49,6 @@ export default function ContactView() {
     }
 
     const getContactPillars = async () => {
-        if (!contactID) {
-            console.warn("contactID is undefined, cannot fetch contact.");
-            return;
-        }
         try {
             const q = query(contactCollection, where('contactUserId', '==', contactID), where('ownerId', '==', user.id));
             const qSnapshot = await getDocs(q);
@@ -54,15 +58,43 @@ export default function ContactView() {
         }
     }
 
+    const renderBackdrop = useCallback(
+    (props: any) => (
+        <BottomSheetBackdrop
+            {...props}
+            disappearsOnIndex={-1}
+            appearsOnIndex={0}
+            onPress={() => bottomSheetRef.current?.close()}
+        />
+    ),
+    []
+);
+
+
     return (
-        <View className="flex-1 flex-col gap-3">
-            <ContactBanner contact={contact as User}/>
-            <View className="flex-1 flex-col">
-                <ContactOption symbol={<FontAwesome name="refresh" size={24} color="black" />} text="Change Pillar" handlePress={() => {console.log("pressed Change Pillar")}}/>
-                <ContactOption symbol={<FontAwesome6 name="people-group" size={24} color="black" />} text="People" handlePress={() => {console.log("pressed People")}}/>
-                <ContactOption symbol={<FontAwesome5 name="lock" size={24} color="black" />} text="Privacy and Security" handlePress={() => {console.log("pressed Priv")}}/>
-                <ContactOption symbol={<FontAwesome6 name="paperclip" size={24} color="black" />} text="Attachments" handlePress={() => {console.log("pressed Attachments")}}/>
+        <GestureHandlerRootView style={{ flex: 1 }}>
+            <View className="flex-1 flex-col gap-3">
+                <ContactBanner contact={contact as User}/>
+                <View className="flex-1 flex-col">
+                    <ContactOption symbol={<FontAwesome name="refresh" size={24} color="black" />} text="Change Pillar" handlePress={handlePresentPress}/>
+                    <ContactOption symbol={<FontAwesome6 name="people-group" size={24} color="black" />} text="People" handlePress={() => {console.log("pressed People")}}/>
+                    <ContactOption symbol={<FontAwesome5 name="lock" size={24} color="black" />} text="Privacy and Security" handlePress={() => {console.log("pressed Priv")}}/>
+                    <ContactOption symbol={<FontAwesome6 name="paperclip" size={24} color="black" />} text="Attachments" handlePress={() => {console.log("pressed Attachments")}}/>
+                </View>
+                <BottomSheet ref={bottomSheetRef} 
+                        index={-1} 
+                        snapPoints={snapPoints} 
+                        enablePanDownToClose={false} 
+                        enableContentPanningGesture={false}
+                        enableHandlePanningGesture={false}
+                        handleComponent={null}
+                        backdropComponent={renderBackdrop}
+                        >
+                            <BottomSheetView className="flex-1 items-center">
+                                <Text>Change Pillar Popup</Text>
+                            </BottomSheetView>
+                    </BottomSheet>  
             </View>
-        </View>
+        </GestureHandlerRootView>
     )
 }
