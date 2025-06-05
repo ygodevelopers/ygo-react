eaf89e641ea6c9541be87aed5472a0552b5b7624
John Mullaney <j.a.mullaney1@gmail.com>
2025-06-05 10:43:57 -0700

    fix: Finish merge with upstream, install missing dependencies

--- app/(app)/(tabs)/pillars.tsx
+++ app/(app)/(tabs)/pillars.tsx
@@ -1,17 +1,9 @@
 import { PillarList } from '@/components/PillarList';
-import { PillarDetail } from '@/components/PillarDetail'
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { PillarStackParamList } from "@/types";
-
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 
 export default function Pillars() {
 
   return (
-    <Stack.Navigator >
-      <Stack.Screen name="Home" component={PillarList} />
-      <Stack.Screen name="Detail" component={PillarDetail} />
-    </Stack.Navigator>
+    <PillarList />
   );
 }
